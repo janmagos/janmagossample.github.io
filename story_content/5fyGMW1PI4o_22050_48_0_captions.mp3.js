6f179b3f589005b875d43be5c46c7a648021bccf
janmagos <janmagos@users.noreply.github.com>
2020-06-21 17:57:45 +0800

    test

---
+++ story_content/5fyGMW1PI4o_22050_48_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.9.21069.0%0D%0A%0D%0A00:00:00.224%20--%3E%2000:00:03.552%0D%0AWelcome%20to%20trial%20101%0D%0A%0D%0A00:00:03.616%20--%3E%2000:00:04.366%0D%0A%0D%0A%0D%0A"}')
